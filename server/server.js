0baad51f24a20efa3fe49a06d4182157faf932d8
Jerred Reed <Jerred Reed>
2017-06-25 12:23:34 -0700

    Added Delete function

--- server/server.js
+++ server/server.js
@@ -48,7 +48,27 @@ app.get('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.send(400).send();
     })
-})
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.send(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.send(400).send();
+    })
+});
+
+
 
 app.listen(port, () => {
     console.log('Server running on port', port);
